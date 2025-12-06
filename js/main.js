2e55cc7005b40eec10ac24b6e18867beedfb2d34
MACCHA-co <133297445+MACCHA-co@users.noreply.github.com>
2025-12-06 19:06:13 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const clientId = "0t9h8td1qfp8phhcen3hd6jaghnafs";
 const redirectUri = "https://MACCHA-co.github.io/voice_transcription/callback.html";
-const channel = "チャンネル名"
+const channel = "ryokumacha"
 let accessToken = localStorage.getItem("twitch_token");
 let twitchUsername = localStorage.getItem("twitch_username");
 

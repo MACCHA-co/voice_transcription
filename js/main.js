f44891b0fe253ba518b53f3c1f5f8d9332ba24eb
MACCHA-co <133297445+MACCHA-co@users.noreply.github.com>
2025-12-06 18:36:20 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const clientId = "0t9h8td1qfp8phhcen3hd6jaghnafs";
-const redirectUri = "https://maccha-co.github.io/voice-chat/callback.html";
+const redirectUri = "https://MACCHA-co.github.io/voice-chat/callback.html";
 
 let accessToken = null;
 
